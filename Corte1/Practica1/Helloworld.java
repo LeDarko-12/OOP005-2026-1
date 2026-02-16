512cc84059ff2029cfd93913bf1755690ccf8980
LeDarko-12 <darkoosi11@gmail.com>
2026-02-15 22:33:10 -0500

    Helloworld.java

--- Corte1/Practica1/Helloworld.java
+++ Corte1/Practica1/Helloworld.java
@@ -1,1 +1,10 @@
+package helloworld;
 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
